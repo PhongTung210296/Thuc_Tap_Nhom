d3349d33a3f7160f0a78d84a18dbaa1de2655519
VuThuy22 <vthuy2222@gmail.com>
2017-05-25 21:26:52 +0700

    Module_DamNhiem

---
+++ Quan_Ly_Nhan_Su/Quan_Ly_Nhan_Su/Model/DamNhiemMod.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using Phần_mềm_quản_lý_nhân_sự_V1._1.Object;
+using Quan_Ly_Nhan_Su.Object;
+
+namespace Quan_Ly_Nhan_Su.Model
+{
+    class DamNhiemMod
+    {
+        ConnectToSql con = new ConnectToSql();
+        SqlCommand cmd = new SqlCommand();
+
+        public DataTable GetData()
+        {
+            DataTable dt = new DataTable();
+            cmd.CommandText = "select * from DamNhiem";
+            cmd.CommandType = CommandType.Text;
+            cmd.Connection = con.strConn;
+            try
+            {
+                con.OpenConnect();
+                SqlDataAdapter sda = new SqlDataAdapter(cmd);
+                sda.Fill(dt);
+                con.CloseConnection();
+            }
+            catch (Exception ex)
+            {
+                string mes = ex.Message;
+                cmd.Dispose();
+                con.CloseConnection();
+            }
+            return dt;
+        }
+
+
+        public bool AddDamNhiem(DamNhiemObj dnobj)
+        {
+            cmd.CommandText = "Insert into DamNhiem values ('" + dnobj.ThoiGianCongTac + "','" + dnobj.MaNV + "','" + dnobj.MaCV + "');";
+            cmd.CommandType = CommandType.Text;
+            cmd.Connection = con.strConn;
+            try
+            {
+                con.OpenConnect();
+                cmd.ExecuteNonQuery();
+                con.CloseConnection();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                string mes = ex.Message;
+                cmd.Dispose();
+                con.CloseConnection();
+            }
+            return true;
+        }
+
+
+        public bool DelDamNhiem(string ma)
+        {
+            cmd.CommandText = "delete DamNhiem where ThoiGianCongTac = '" + ma + "'";
+            cmd.CommandType = CommandType.Text;
+            cmd.Connection = con.strConn;
+            try
+            {
+                con.OpenConnect();
+                cmd.ExecuteNonQuery();
+                con.CloseConnection();
+            }
+            catch (Exception ex)
+            {
+                string mes = ex.Message;
+                cmd.Dispose();
+                con.CloseConnection();
+            }
+            return true;
+        }
+
+
+        public bool UpdateDamNhiem(DamNhiemObj dnobj)
+        {
+            cmd.CommandText = " update DamNhiem set ThoiGianCongTac='" + dnobj.ThoiGianCongTac + "',MaNV='" + dnobj.MaNV + "',MaCV='" + dnobj.MaCV + "'";
+            cmd.CommandType = CommandType.Text;
+            cmd.Connection = con.strConn;
+            try
+            {
+                con.OpenConnect();
+                cmd.ExecuteNonQuery();
+                con.CloseConnection();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                string mes = ex.Message;
+                cmd.Dispose();
+                con.CloseConnection();
+            }
+            return true;
+        }
+
+        public DataTable SeachDamNhiem(string maDamNhiem)
+        {
+            DataTable dt = new DataTable();
+            cmd.CommandText = "select * from DamNhiem  where ThoiGianCongTac like '%" + maDamNhiem + "%'";
+            cmd.CommandType = CommandType.Text;
+            cmd.Connection = con.strConn;
+            try
+            {
+                con.OpenConnect();
+                SqlDataAdapter sda = new SqlDataAdapter(cmd);
+                sda.Fill(dt);
+                con.CloseConnection();
+                return dt;
+
+            }
+            catch (Exception ex)
+            {
+                string mes = ex.Message;
+                cmd.Dispose();
+                con.CloseConnection();
+            }
+            return dt;
+        }
+    }
+}
---
+++ Quan_Ly_Nhan_Su/Quan_Ly_Nhan_Su/View/frmDamNhiem.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Quan_Ly_Nhan_Su.Model;
+using Quan_Ly_Nhan_Su.Object;
+
+namespace Quan_Ly_Nhan_Su.View
+{
+    public partial class frmDamNhiem : Form
+    {
+        public frmDamNhiem()
+        {
+            InitializeComponent();
+        }
+        DamNhiemMod dnctl = new DamNhiemMod();
+        DamNhiemObj dnobj = new DamNhiemObj();
+        int flag = 0;
+        public void dis_en(bool e)
+        {
+            txtThoiGian.Enabled = e;
+            cbbMaCV.Enabled = e;
+            cbbMaNV.Enabled = e;
+            btnHuy.Enabled = e;
+            btnLuu.Enabled = e;
+            btnThem.Enabled = !e;
+            btnSua.Enabled = !e;
+            btnXoa.Enabled = !e;
+        }
+
+        private void clean()
+        {
+            txtThoiGian.Clear();
+        }
+        private void GanDuLieu(DamNhiemObj dn1obj)
+        {
+            dn1obj.ThoiGianCongTac = txtThoiGian.Text.ToString().Trim();
+            dn1obj.MaNV = cbbMaNV.Text.ToString().Trim();
+            dn1obj.MaCV = cbbMaCV.Text.ToString().Trim();
+        }
+
+        private void frmDamNhiem_Load(object sender, EventArgs e)
+        {
+
+            dgvDamNhiem.DataSource = dnctl.GetData();
+            dgvDamNhiem.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dis_en(false);
+        }
+
+        private void dgvDamNhiem_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0 && e.ColumnIndex >= 0)
+            {
+                try
+                {
+                    txtThoiGian.Text = dgvDamNhiem.CurrentRow.Cells[0].Value.ToString();
+                    cbbMaNV.Text = dgvDamNhiem.CurrentRow.Cells[1].Value.ToString();
+                    cbbMaCV.Text = dgvDamNhiem.CurrentRow.Cells[2].Value.ToString();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+
+        private void btnThem_Click(object sender, EventArgs e)
+        {
+            flag = 0;
+            clean();
+            dis_en(true);
+        }
+
+        private void btnSua_Click(object sender, EventArgs e)
+        {
+            flag = 1;
+            dis_en(true);
+        }
+
+        private void btnXoa_Click(object sender, EventArgs e)
+        {
+            DialogResult dr = MessageBox.Show("Bạn có thật sự muốn xóa ?", "Thông Báo", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            {
+                if (dr == DialogResult.Yes)
+                {
+                    if (dnctl.DelDamNhiem(txtThoiGian.Text.Trim()))
+                    {
+                        MessageBox.Show("Xóa thành công", "Thông Báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        frmDamNhiem_Load(sender, e);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Xóakhông thành công", "Thông Báo", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+                else
+                {
+                    return;
+                }
+            }
+        }
+
+        private void btnLuu_Click(object sender, EventArgs e)
+        {
+            GanDuLieu(dnobj);
+            if (flag == 0)   // thêm
+            {
+                if (dnctl.AddDamNhiem(dnobj))
+                {
+                    MessageBox.Show("Thêm thành công", "Thông Báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    frmDamNhiem_Load(sender, e);
+                }
+                else
+                {
+                    MessageBox.Show("Thêm không thành công", "Thông Báo", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else            // sửa
+            {
+                if (dnctl.UpdateDamNhiem(dnobj))
+                {
+                    MessageBox.Show("Sửa thành công", "Thông Báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    frmDamNhiem_Load(sender, e);
+                }
+                else
+                {
+                    MessageBox.Show("Sửa không thành công", "Thông Báo", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void btnHuy_Click(object sender, EventArgs e)
+        {
+            frmDamNhiem_Load(sender, e);
+            dis_en(false);
+        }
+    }
+}
