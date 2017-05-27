1333f1093b080d6641d127df397bd53a1a171afc
VuThuy22 <vthuy2222@gmail.com>
2017-05-27 17:22:22 +0700

    hh

--- Quan_Ly_Nhan_Su/Quan_Ly_Nhan_Su/Model/ChucVuMod.cs
+++ Quan_Ly_Nhan_Su/Quan_Ly_Nhan_Su/Model/ChucVuMod.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Quan_Ly_Nhan_Su.Object;
 
 namespace Quan_Ly_Nhan_Su.Model
 {
