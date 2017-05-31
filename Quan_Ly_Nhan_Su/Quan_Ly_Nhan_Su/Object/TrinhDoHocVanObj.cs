89b7c074b8d209386b9cacab3759fd7958676377
manhhiep1996 <manhhiep1996@gmail.com>
2017-06-01 00:32:10 +0700

    fix

--- Quan_Ly_Nhan_Su/Quan_Ly_Nhan_Su/Object/TrinhDoHocVanObj.cs
+++ Quan_Ly_Nhan_Su/Quan_Ly_Nhan_Su/Object/TrinhDoHocVanObj.cs
@@ -6,20 +6,5 @@ using System.Threading.Tasks;
 
 namespace Quan_Ly_Nhan_Su.Object
 {
-    class TrinhDoHocVanObj
-    {
-        public string MaTDHV { get; set; }
-        public string TenTDHV { get; set; }
-        public string ChuyenNganh { get; set; }
-        public TrinhDoHocVanObj()
-        {
-
-        }
-        public TrinhDoHocVanObj(string matdhv, string tentdv, string chuyennganh)
-        {
-            this.MaTDHV = matdhv;
-            this.TenTDHV = tentdv;
-            this.ChuyenNganh = chuyennganh;
-        }
-    }
+    
 }
